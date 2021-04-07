f5df3f6e9d901ec175c5deffb892c982564f2ad1
akasmirhan4 <amirrul.kasmirhan@gmail.com>
2021-04-07 10:44:15 +0800

    Added comments and removing unwanted functions

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-var targetSelection = null;
 var dpi = 300;
 var notepad;
 const NOTEPAD = $(".text-area");
@@ -6,13 +5,12 @@ const RULERX = $(".ruler.horizontal .inner");
 const RULERY = $(".ruler.vertical .inner");
 
 $(document).ready(function () {
-
     // Initialise Notepad
     var width = localStorage.textAreaWidth ?? 900;
     var height = localStorage.textAreaHeight ?? 600;
     initNotepad(parseInt(width), parseInt(height));
 
-    // Set font-family to its style
+    // Set font-family options to its style
     for (var i = 0; i < $(".font-family option").length; i++) {
         let fontFamily = $(".font-family option")[i].innerHTML;
         $(".font-family option")[i].style["font-family"] = fontFamily;
@@ -21,17 +19,18 @@ $(document).ready(function () {
     // Set highlight none
     $(".font-highlight button:first-child i").css("color", "rgba(0,0,0,0.1)");
 
+    // Check localstorage for saved data and format
     if (localStorage.textAreaData !== undefined) {
         notepad.node.html(localStorage.textAreaData);
         $(".filename").val(localStorage.filename);
         wordsCount();
 
         if (localStorage.textAreaData != notepad.node.html()) {
-            $('#save i').addClass('shake');
-            $('#save i').css('color', "rgb(33, 37, 41)");
+            $("#save i").addClass("shake");
+            $("#save i").css("color", "rgb(33, 37, 41)");
         } else {
-            $('#save i').removeClass('shake');
-            $('#save i').css('color', "rgba(0,0,0,0.1)");
+            $("#save i").removeClass("shake");
+            $("#save i").css("color", "rgba(0,0,0,0.1)");
         }
     }
 });
@@ -39,51 +38,11 @@ $(document).ready(function () {
 function initNotepad(width, height) {
     NOTEPAD.html("");
     notepad = new Notepad(NOTEPAD, 50, 50, width, height, RULERX, RULERY);
-
 }
 
-// Set style of element
-function setStyle(elem, style) {
-    elem.attr("size", style["font-size"]);
-    elem.attr("face", style["font-family"]);
-    elem.attr("face", style["font-family"]);
-
-    elem.css({
-        size: style["font-size"],
-        "font-family": style["font-family"],
-        "font-weight": style["isBold"] ? "bold" : "normal", //ternary condition
-        "font-style": style["isItalic"] ? "italic" : "normal", //ternary condition
-        "text-decoration": style["isUnderline"] ? "underline" : "none", //ternary condition
-        color: style["font-color"],
-        background: style["font-highlight"],
-    });
-    // elem.parent("div").css({
-    //     'text-align': style['text-alignment']
-    // })
-}
-
-// Get style of current notepad format
-function getCurrentStyle() {
-    let currentStyle = {};
-    currentStyle["font-size"] = $("#font-size option:selected")[0].value;
-    currentStyle["font-family"] = $("#font-family option:selected")[0].value;
-    currentStyle["isBold"] = $("#bold").hasClass("active");
-    currentStyle["isItalic"] = $("#italic").hasClass("active");
-    currentStyle["isUnderline"] = $("#underline").hasClass("active");
-    currentStyle["font-color"] = $("#font-color")[0].value;
-    let isHighlighting =
-        $(".font-highlight button:first-child i")[0].style["color"] !==
-        "rgba(0, 0, 0, 0.1)";
-    currentStyle["font-highlight"] = isHighlighting
-        ? $("#font-highlight")[0].value
-        : "unset";
-    currentStyle["text-alignment"] = $(
-        ".text-alignment input[type='radio']:checked"
-    ).attr("value");
-    return currentStyle;
-}
 /* --------- START BUTTON LISTENER --------- */
 
+// New button
 $("#new").mouseup(function () {
     if (confirm("Are you sure you want to create a new document?")) {
         $(".filename").val("");
@@ -93,6 +52,7 @@ $("#new").mouseup(function () {
     }
 });
 
+// Save button
 $("#save").mouseup(function () {
     localStorage.setItem("textAreaData", notepad.node.html());
     localStorage.setItem("filename", $(".filename").val());
@@ -100,8 +60,7 @@ $("#save").mouseup(function () {
     localStorage.setItem("textAreaHeight", notepad.node.outerHeight());
 });
 
-$("#upload").mouseup(function () { });
-
+// Download button
 $("#download").mouseup(function () {
     $(document).googoose({
         area: ".text-area",
@@ -111,26 +70,32 @@ $("#download").mouseup(function () {
     });
 });
 
+// Undo button
 $("#undo").mouseup(function () {
     document.execCommand("undo");
 });
 
+// Redo button
 $("#redo").mouseup(function () {
     document.execCommand("redo");
 });
 
+// Cut button
 $("#cut").mouseup(function () {
     document.execCommand("cut");
 });
 
+// Copy button
 $("#copy").mouseup(function () {
     document.execCommand("copy");
 });
 
+// Paste button
 $("#paste").mouseup(function () {
     document.execCommand("paste");
 });
 
+// Select All button
 $("#select-all").mouseup(function () {
     $(".text-area")[0].focus();
     document.execCommand("selectAll");
@@ -142,21 +107,12 @@ $("#select-all").mouseup(function () {
 
 //font-size
 $("#font-size").on("change", function () {
-    document.execCommand(
-        "fontSize",
-        false,
-        $("#font-size option:selected")[0].value
-    );
-    // $(getSelection().anchorNode.parentNode.parentNode).removeAttr("size").css("font-size", $('#font-size option:selected')[0].value + "px");
+    document.execCommand("fontSize", false, $("#font-size option:selected")[0].value);
 });
 
 //font-family
 $("#font-family").on("change", function () {
-    document.execCommand(
-        "fontName",
-        false,
-        $("#font-family option:selected")[0].value
-    );
+    document.execCommand("fontName", false, $("#font-family option:selected")[0].value);
 });
 
 //font-color
@@ -167,10 +123,7 @@ $("#font-color").on("input", function () {
 
 //font-highlight
 $("#font-highlight").on("input", function () {
-    $(".font-highlight button:first-child i").css(
-        "color",
-        $("#font-highlight")[0].value
-    );
+    $(".font-highlight button:first-child i").css("color", $("#font-highlight")[0].value);
     document.execCommand("backColor", false, $("#font-highlight")[0].value);
 });
 
@@ -191,11 +144,10 @@ $("#italic").mouseup(function () {
 $("#underline").mouseup(function () {
     document.execCommand("underline");
 });
+
 //text-alignment
 $(".text-alignment").on("change", function () {
-    let alignment = $(".text-alignment input[type='radio']:checked").attr(
-        "value"
-    );
+    let alignment = $(".text-alignment input[type='radio']:checked").attr("value");
     if (alignment == "left") {
         document.execCommand("justifyLeft");
     } else if (alignment == "right") {
@@ -211,71 +163,22 @@ $(".text-alignment").on("change", function () {
 
 /* --------- END STYLE CHANGE LISTENER --------- */
 
-function getTextChildNode(node) {
-    var textNode = [];
-    // Loop through all siblings
-    for (var n = 0; n < node.childNodes.length; n++) {
-        if (node.childNodes[n].nodeType == 1) {
-            // Iterative function
-            var tempNodes = getTextChildNode(node.childNodes[n]);
-            textNode = textNode.concat(tempNodes);
-
-        } else if (node.childNodes[n].nodeType == 3) {
-            textNode.push(node.childNodes[n]);
-        }
-    }
-    return textNode;
-}
-
-// Get all text nodes in text area
-function getAllTextNodes() {
-    var nodes = notepad.node.find(":not(iframe)").addBack().contents().filter(function () {
-        return this.nodeType == 3;
-    });
-    return nodes;
-}
-
-// Get all text nodes in selected text
-function getTextNodes() {
-    var allNodes = getAllTextNodes();
-    var selection = getSelection();
-
-    var nodes = [];
-
-    // If selection is empty
-    if (selection.type == "None") {
-        return null;
-    } else {
-
-        // Loop through all text nodes in text-area
-        for (var n = 0; n < allNodes.length; n++) {
-
-            //If selection contain a text node, push into the result
-            if (selection.containsNode(getAllTextNodes()[n])) {
-                nodes.push(getAllTextNodes()[n]);
-            }
-        }
-        return nodes;
-    }
-}
 
 /* --------- TEXT AREA FOCUS LISTENER --------- */
 
 function updateTextArea() {
     // update save button
     if (localStorage.textAreaData != notepad.node.html()) {
-        $('#save i').addClass('shake');
-        $('#save i').css('color', "rgb(33, 37, 41)");
+        $("#save i").addClass("shake");
+        $("#save i").css("color", "rgb(33, 37, 41)");
     } else {
-        $('#save i').removeClass('shake');
-        $('#save i').css('color', "rgba(0,0,0,0.1)");
+        $("#save i").removeClass("shake");
+        $("#save i").css("color", "rgba(0,0,0,0.1)");
     }
     wordsCount();
     checkSelectionFormat();
     ee();
-
 }
-
 
 $("html").mouseup(function (e) {
     updateTextArea();
@@ -285,34 +188,7 @@ $("html").keyup(function () {
     updateTextArea();
 });
 
-
 /* ---------- START CHECK FORMAT ---------- */
-
-function stateCheck(style, currentState, ACTIVE, NORMAL) {
-    if (currentState != 2) {
-        if (style == ACTIVE) {
-
-            // stop once there are text that is mixed state
-            if (currentState == 0) {
-                return 2;
-            }
-            else {
-                return 1;
-            }
-            // stop once there are text that is mixed state
-        } else if (style == NORMAL) {
-            if (currentState == 1) {
-                return 2;
-            }
-            else {
-                return 0;
-            }
-        } else {
-            console.error("style not consistent");
-            return -1;
-        }
-    }
-}
 
 function checkSelectionFormat() {
     const BOLD_ACTIVE = 700;
@@ -325,25 +201,25 @@ function checkSelectionFormat() {
     const UNDERLINE_NORMAL = "none";
 
     /* 
-  
-      STATE:
-  
-      -1 : ERROR
-      0 : SINGLE
-      1 : MIXED
-  
-    */
+    
+        STATE:
+    
+        -1 : ERROR
+        0 : SINGLE
+        1 : MIXED
+    
+      */
 
     var states = {
-        'fontFamily': -1,
-        'fontSize': -1,
-        'color': -1,
-        'backgroundColor': -1,
-        'bold': -1,
-        'italic': -1,
-        'underline': -1,
-        'alignment': -1,
-    }
+        fontFamily: -1,
+        fontSize: -1,
+        color: -1,
+        backgroundColor: -1,
+        bold: -1,
+        italic: -1,
+        underline: -1,
+        alignment: -1,
+    };
 
     var nodes = getTextNodes();
     if (nodes == null || nodes.length == 0) {
@@ -378,7 +254,6 @@ function checkSelectionFormat() {
     for (var i = 0; i < nodes.length; i++) {
         //if node is #text
         if (nodes[i]["nodeType"] == 3) {
-
             // Check for font-family state
             if ($(nodes[i].parentNode).css("font-family") != initFontFamily) {
                 states.fontFamily = 1;
@@ -395,7 +270,9 @@ function checkSelectionFormat() {
             }
 
             // Check for background-color state
-            if ($(nodes[i].parentNode).css("background-color") != initBackgroundColor) {
+            if (
+                $(nodes[i].parentNode).css("background-color") != initBackgroundColor
+            ) {
                 states.backgroundColor = 1;
             }
 
@@ -418,17 +295,14 @@ function checkSelectionFormat() {
             if ($(nodes[i].parentNode).css("text-align") != initAlignment) {
                 states.alignment = 1;
             }
-
-
         }
     }
 
     // Update notepad style options
     if (states.fontFamily == 0) {
-        $('#font-family').val(initFontFamily);
-    }
-    else {
-        $('#font-family').val("");
+        $("#font-family").val(initFontFamily);
+    } else {
+        $("#font-family").val("");
     }
 
     if (states.fontSize == 0) {
@@ -455,32 +329,31 @@ function checkSelectionFormat() {
                 initFontSize = 7;
                 break;
         }
-        $('#font-size').val(initFontSize);
-    }
-    else {
-        $('#font-size').val("");
+        $("#font-size").val(initFontSize);
+    } else {
+        $("#font-size").val("");
     }
 
     if (states.color == 0) {
         $("#font-color").val(initColor);
         $(".font-color i").css("color", initColor);
-    }
-    else {
+    } else {
         $("#font-color").val("");
         $(".font-color i").css("color", "rgba(0,0,0,0.1)");
     }
 
     if (states.backgroundColor == 0) {
-        if (nodes[0].parentNode.id == "text-area" || $(nodes[0]).parent().css("background-color") == "rgba(0, 0, 0, 0)") {
+        if (
+            nodes[0].parentNode.id == "text-area" ||
+            $(nodes[0]).parent().css("background-color") == "rgba(0, 0, 0, 0)"
+        ) {
             $("#font-highlight").val("");
             $("#font-highlight i").css("color", "rgba(0,0,0,0.1)");
-        }
-        else {
+        } else {
             $("#font-highlight").val(initBackgroundColor);
             $("#font-highlight i").css("color", initBackgroundColor);
         }
-    }
-    else {
+    } else {
         $("#font-highlight").val("");
         $("#font-highlight i").css("color", "rgba(0,0,0,0.1)");
     }
@@ -496,15 +369,13 @@ function checkSelectionFormat() {
             $("#bold").removeClass("active");
             $("#bold").attr("aria-pressed", "false");
             $("#bold i").css("color", "rgb(33, 37, 41)");
-        }
-        else {
+        } else {
             // ERROR
             $("#bold").removeClass("active");
             $("#bold").attr("aria-pressed", "false");
             $("#bold i").css("color", "rgba(0,0,0,0.1)");
         }
-    }
-    else {
+    } else {
         // MIXED
         $("#bold").removeClass("active");
         $("#bold").attr("aria-pressed", "false");
@@ -522,15 +393,13 @@ function checkSelectionFormat() {
             $("#italic").removeClass("active");
             $("#italic").attr("aria-pressed", "false");
             $("#italic i").css("color", "rgb(33, 37, 41)");
-        }
-        else {
+        } else {
             // ERROR
             $("#italic").removeClass("active");
             $("#italic").attr("aria-pressed", "false");
             $("#italic i").css("color", "rgba(0,0,0,0.1)");
         }
-    }
-    else {
+    } else {
         // MIXED
         $("#italic").removeClass("active");
         $("#italic").attr("aria-pressed", "false");
@@ -548,15 +417,13 @@ function checkSelectionFormat() {
             $("#underline").removeClass("active");
             $("#underline").attr("aria-pressed", "false");
             $("#underline i").css("color", "rgb(33, 37, 41)");
-        }
-        else {
+        } else {
             // ERROR
             $("#underline").removeClass("active");
             $("#underline").attr("aria-pressed", "false");
             $("#underline i").css("color", "rgba(0,0,0,0.1)");
         }
-    }
-    else {
+    } else {
         // MIXED
         $("#underline").removeClass("active");
         $("#underline").attr("aria-pressed", "false");
@@ -564,29 +431,26 @@ function checkSelectionFormat() {
     }
 
     if (states.alignment == 0) {
-        $(".text-alignment input:checked").parent().removeClass('active');
+        $(".text-alignment input:checked").parent().removeClass("active");
         $(".text-alignment input:checked").attr("checked", false);
         $(".text-alignment [value='" + initAlignment + "']").attr("checked", true);
-        $(".text-alignment input:checked").parent().addClass('active');
-    }
-    else {
+        $(".text-alignment input:checked").parent().addClass("active");
+    } else {
         // ERROR OR MIXED
-        $(".text-alignment input").parent().removeClass('active');
+        $(".text-alignment input").parent().removeClass("active");
     }
 
     return states;
 }
 /* ---------- END CHECK FORMAT ---------- */
 
-
 // Make header position fixed at certain scrollpoint
-$('body').scroll(function () {
-    var scrollTop = $('body').scrollTop();
+$("body").scroll(function () {
+    var scrollTop = $("body").scrollTop();
     var targetScrollPoint = $("header").outerHeight();
     if (scrollTop > targetScrollPoint) {
         $("header").css("position", "fixed");
-    }
-    else {
+    } else {
         $("header").css("position", "initial");
     }
 });
--- js/utility.js
+++ js/utility.js
@@ -1,3 +1,38 @@
+
+// Get all text nodes in text area
+function getAllTextNodes() {
+    var nodes = notepad.node
+        .find(":not(iframe)")
+        .addBack()
+        .contents()
+        .filter(function () {
+            return this.nodeType == 3;
+        });
+    return nodes;
+}
+
+// Get all text nodes in selected text
+function getTextNodes() {
+    var allNodes = getAllTextNodes();
+    var selection = getSelection();
+
+    var nodes = [];
+
+    // If selection is empty
+    if (selection.type == "None") {
+        return null;
+    } else {
+        // Loop through all text nodes in text-area
+        for (var n = 0; n < allNodes.length; n++) {
+            //If selection contain a text node, push into the result
+            if (selection.containsNode(getAllTextNodes()[n])) {
+                nodes.push(getAllTextNodes()[n]);
+            }
+        }
+        return nodes;
+    }
+}
+
 function wordsCount() {
     var content = $(".text-area")[0].innerText.replace(/\n/ig, ' ').replaceAll(/\s+/g, ' ').trim();
     if (content) {
